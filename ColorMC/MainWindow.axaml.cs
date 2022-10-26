4d8cfb7a7da455c6c16aa15f9cb0c4554e1f3335
Color_yr <402067010@qq.com>
2022-10-26 15:54:11 +0800

    up

---
+++ ColorMC/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ColorMC
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# ColorMC
+一个全新的全平台启动器
+基于[NsisoLauncher](https://github.com/Coloryr/NsisoLauncher-1)
