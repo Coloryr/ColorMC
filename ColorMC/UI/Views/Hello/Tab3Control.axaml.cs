a5175bd1e949f3618e306e1c0b376e2d70107190
Color_yr <402067010@qq.com>
2022-10-28 17:36:26 +0800

    java

--- ColorMC/UI/Views/Hello/Tab3Control.axaml.cs
+++ ColorMC/UI/Views/Hello/Tab3Control.axaml.cs
@@ -4,8 +4,14 @@ namespace ColorMC.UI.Views.Hello;
 
 public partial class Tab3Control : UserControl
 {
+    private HelloWindow Window;
     public Tab3Control()
     {
         InitializeComponent();
+    }
+
+    public void SetWindow(HelloWindow window)
+    {
+        Window = window;
     }
 }
