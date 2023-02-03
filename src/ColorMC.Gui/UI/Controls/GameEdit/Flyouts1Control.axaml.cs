09080034348ef3e83938bd8516c74c682a5ef487
Color_yr <402067010@qq.com>
2023-02-03 17:27:33 +0800

    up

--- src/ColorMC.Gui/UI/Controls/GameEdit/Flyouts1Control.axaml.cs
+++ src/ColorMC.Gui/UI/Controls/GameEdit/Flyouts1Control.axaml.cs
@@ -18,6 +18,13 @@ public partial class Flyouts1Control : UserControl
         Button1.Click += Button1_Click;
         Button2.Click += Button2_Click;
         Button3.Click += Button3_Click;
+        Button4.Click += Button4_Click;
+    }
+
+    private void Button4_Click(object? sender, RoutedEventArgs e)
+    {
+        FlyoutBase.Hide();
+        GameBinding.OpenMcmod(Obj);
     }
 
     private void Button3_Click(object? sender, RoutedEventArgs e)
