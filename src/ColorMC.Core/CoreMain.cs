3fdeb05a9eb89b6fb0b5bb810e22afd51174e8b5
Coloryr <402067010@qq.com>
2023-04-28 23:22:54 +0800

    up
    version

--- src/ColorMC.Core/CoreMain.cs
+++ src/ColorMC.Core/CoreMain.cs
@@ -10,7 +10,7 @@ namespace ColorMC.Core;
 
 public static class ColorMCCore
 {
-    public const string Version = "A17.230424";
+    public const string Version = "A17.230428";
 
     /// <summary>
     /// 运行路径
