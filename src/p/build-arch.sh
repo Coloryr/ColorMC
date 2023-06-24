9d88625188195e50f414f33cf10a28cc7e8519b9
Coloryr <402067010@qq.com>
2023-06-24 14:27:23 +0800

    up

--- src/p/build-arch.sh
+++ src/p/build-arch.sh
@@ -1,2 +1,2 @@
 sudo debtap colormc-A19-linux-amd64.deb
-sudo pacman -U colormc-1-1-x86_64.pkg.tar.zst
+sudo pacman -U colormc-A19-x86_64.pkg.tar.zst
--- src/p/build-ubuntu.sh
+++ src/p/build-ubuntu.sh
@@ -1,3 +1,3 @@
-sudo dpkg -b linux-amd64 colormc-linux-amd64.deb
-sudo dpkg -b linux-arm64 colormc-linux-arm64.deb
+sudo dpkg -b linux-amd64 colormc-A19-linux-amd64.deb
+sudo dpkg -b linux-arm64 colormc-A19-linux-arm64.deb
 sudo ./deb2appimage-0.0.5-x86_64.AppImage -j appimg-amd64.json -o ./
