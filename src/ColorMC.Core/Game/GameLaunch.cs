dd090f877eb456d2224343b13864df37d9cfb77b
Color_yr <402067010@qq.com>
2023-10-03 22:46:58 +0800

    up
    修复1.7.10无法启动

--- src/ColorMC.Core/Game/GameLaunch.cs
+++ src/ColorMC.Core/Game/GameLaunch.cs
@@ -683,7 +683,8 @@ public static class Launch
             if (item.Later == null)
             {
                 //不添加lwjgl
-                if (item.Name.Contains("lwjgl") && (SystemInfo.Os == OsType.Android
+                if ((item.Name.Contains("org/lwjgl") || item.Name.Contains("org\\lwjgl")) 
+                    && (SystemInfo.Os == OsType.Android
                     || (CheckHelpers.IsGameLaunchVersion120(obj.Version)
                         && (obj.Loader == Loaders.Forge || obj.Loader == Loaders.NeoForge))))
                 {
