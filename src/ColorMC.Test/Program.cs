d1b90badc03ffe6b19892c845eed117981f08503
Color_yr <402067010@qq.com>
2023-10-11 15:27:32 +0800

    up
    一个小问题

--- src/ColorMC.Test/Program.cs
+++ src/ColorMC.Test/Program.cs
@@ -44,7 +44,7 @@ internal class Program
         ColorMCCore.AuthStateUpdate = AuthStateUpdate;
         ColorMCCore.GameLog = Log;
 
-        TestItem.Item24();
+        //TestItem.Item24();
 
         GetSha1();
 
