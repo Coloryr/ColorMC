c7b942c934b5cfaf0c5388d08c880e0c23c1aa31
Color_yr <402067010@qq.com>
2023-11-26 16:18:45 +0800

    将更新日志改为倒叙

--- src/ColorMC.Core/CoreMain.cs
+++ src/ColorMC.Core/CoreMain.cs
@@ -13,7 +13,7 @@ namespace ColorMC.Core;
 public static class ColorMCCore
 {
     public const string TopVersion = "A22";
-    public const string DateVersion = "20231125";
+    public const string DateVersion = "20231126";
 
     public const string Version = $"{TopVersion}.{DateVersion}";
 
