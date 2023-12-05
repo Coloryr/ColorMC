524fe6963535d14db5d753c2845f8c9a5978604d
Color_yr <402067010@qq.com>
2023-12-05 22:19:52 +0800

    [up]抽象菜单类

--- src/ColorMC.Gui/UI/Controls/Setting/SettingControl.axaml.cs
+++ src/ColorMC.Gui/UI/Controls/Setting/SettingControl.axaml.cs
@@ -1,84 +1,35 @@
 using Avalonia.Controls;
-using Avalonia.Input;
-using Avalonia.Threading;
 using ColorMC.Gui.Objs;
 using ColorMC.Gui.UI.Model;
 using ColorMC.Gui.UI.Model.Setting;
-using ColorMC.Gui.UI.Windows;
 using System.ComponentModel;
-using System.Threading;
 
 namespace ColorMC.Gui.UI.Controls.Setting;
 
-public partial class SettingControl : UserControl, IUserControl
+public partial class SettingControl : MenuControl
 {
-    private Tab1Control _tab1 = new();
-    private Tab2Control _tab2 = new();
-    private Tab3Control _tab3 = new();
-    private Tab4Control _tab4 = new();
-    private Tab5Control _tab5 = new();
-    private Tab6Control _tab6 = new();
-    private Tab7Control _tab7 = new();
+    private readonly Tab1Control _tab1 = new();
+    private readonly Tab2Control _tab2 = new();
+    private readonly Tab3Control _tab3 = new();
+    private readonly Tab4Control _tab4 = new();
+    private readonly Tab5Control _tab5 = new();
+    private readonly Tab6Control _tab6 = new();
+    private readonly Tab7Control _tab7 = new();
 
-    private bool _switch1 = false;
+    public override string Title => App.Lang("SettingWindow.Title");
 
-    private CancellationTokenSource _cancel = new();
-    private CancellationTokenSource _cancel1 = new();
-
-    private int _now;
-
-    public IBaseWindow Window => App.FindRoot(VisualRoot);
-
-    public string Title => App.Lang("SettingWindow.Title");
-
-    public string UseName { get; }
+    public override string UseName { get; }
 
     public SettingControl()
     {
         InitializeComponent();
 
         UseName = ToString() ?? "SettingControl";
-
-        StackPanel1.PointerPressed += StackPanel1_PointerPressed;
-        StackPanel2.PointerPressed += StackPanel2_PointerPressed;
     }
 
-    private void StackPanel2_PointerPressed(object? sender, PointerPressedEventArgs e)
-    {
-        (DataContext as SettingModel)!.OpenSide();
-    }
-
-    private void StackPanel1_PointerPressed(object? sender, PointerPressedEventArgs e)
-    {
-        (DataContext as SettingModel)!.CloseSide();
-    }
-
-    public void Closed()
+    public override void Closed()
     {
         App.SettingWindow = null;
-    }
-
-    private void Go(UserControl to)
-    {
-        _cancel.Cancel();
-        _cancel.Dispose();
-
-        _cancel = new();
-
-        var model = (DataContext as SettingModel)!;
-
-        if (!_switch1)
-        {
-            Content2.Child = to;
-            _ = App.PageSlide500.Start(Content1, Content2, _now < model.NowView, _cancel.Token);
-        }
-        else
-        {
-            Content1.Child = to;
-            _ = App.PageSlide500.Start(Content2, Content1, _now < model.NowView, _cancel.Token);
-        }
-
-        _switch1 = !_switch1;
     }
 
     public async void GoTo(SettingType type)
@@ -96,19 +47,12 @@ public partial class SettingControl : UserControl, IUserControl
         }
     }
 
-    public void Opened()
+    public override void Opened()
     {
         Window.SetTitle(Title);
 
         Content1.Child = _tab2;
         (DataContext as SettingModel)!.LoadUISetting();
-    }
-
-    public void SetBaseModel(BaseModel model)
-    {
-        var amodel = new SettingModel(model);
-        amodel.PropertyChanged += Amodel_PropertyChanged;
-        DataContext = amodel;
     }
 
     private void Amodel_PropertyChanged(object? sender, PropertyChangedEventArgs e)
@@ -132,61 +76,42 @@ public partial class SettingControl : UserControl, IUserControl
                     break;
             }
         }
-        else if (e.PropertyName == "NowView")
-        {
-            var model = (DataContext as SettingModel)!;
-            switch (model.NowView)
-            {
-                case 0:
-                    Go(_tab2);
-                    model.LoadUISetting();
-                    break;
-                case 1:
-                    Go(_tab3);
-                    model.LoadHttpSetting();
-                    break;
-                case 2:
-                    Go(_tab4);
-                    model.LoadArg();
-                    break;
-                case 3:
-                    Go(_tab5);
-                    model.LoadJava();
-                    break;
-                case 4:
-                    Go(_tab6);
-                    model.LoadServer();
-                    break;
-                case 5:
-                    Go(_tab1);
-                    break;
-                case 6:
-                    Go(_tab7);
-                    _tab7.Start();
-                    break;
-            }
+    }
 
-            _now = model.NowView;
-        }
-        else if (e.PropertyName == "SideOpen")
-        {
-            _cancel1.Cancel();
-            _cancel1.Dispose();
-            _cancel1 = new();
+    protected override MenuModel SetModel(BaseModel model)
+    {
+        var amodel = new SettingModel(model);
+        amodel.PropertyChanged += Amodel_PropertyChanged;
+        return amodel;
+    }
 
-            StackPanel1.IsVisible = true;
-            Dispatcher.UIThread.Post(() =>
-            {
-                App.SidePageSlide300.Start(null, DockPanel1, _cancel1.Token);
-            });
-        }
-        else if (e.PropertyName == "SideClose")
+    protected override Control ViewChange(int index)
+    {
+        var model = (DataContext as SettingModel)!;
+        switch (index)
         {
-            _cancel1.Cancel();
-            _cancel1.Dispose();
-            _cancel1 = new();
-            App.SidePageSlide300.Start(DockPanel1, null, _cancel1.Token);
-            StackPanel1.IsVisible = false;
+            case 0:
+                model.LoadUISetting();
+                return _tab2;
+            case 1:
+                model.LoadHttpSetting();
+                return _tab3;
+            case 2:
+                model.LoadArg();
+                return _tab4;
+            case 3:
+                model.LoadJava();
+                return _tab5;
+            case 4:
+                model.LoadServer();
+                return _tab6;
+            case 5:
+                return _tab1;
+            case 6:
+                _tab7.Start();
+                return _tab7;
+            default:
+                throw new InvalidEnumArgumentException();
         }
     }
 }
