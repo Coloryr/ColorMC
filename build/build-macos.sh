e69a7ee2c249acb345574a05ea5ce46c9dd212d3
Color_yr <402067010@qq.com>
2024-01-24 20:31:25 +0800

    Update build-macos.sh

--- build/build-macos.sh
+++ build/build-macos.sh
@@ -7,17 +7,17 @@ do
     version=$line
 done
 
-echo "ColorMC build macos-amd64 version: $version"
+echo "ColorMC build osx-x64 version: $version"
 
 mkdir ./build_out
 
-base=./src/build_out/osx64-dotnet
+base=./src/build_out/osx-x64-dotnet
 base_dir="$base/ColorMC.app/Contents"
 
 rm $zip_name
 rm -rf $base
 
-dotnet publish ./src/ColorMC.Launcher -p:PublishProfile=Osx-x64
+dotnet publish ./src/ColorMC.Launcher -p:PublishProfile=osx-x64
 
 mkdir $base/ColorMC.app
 mkdir $base_dir
@@ -40,10 +40,10 @@ done
 
 chmod a+x $base_dir/$dir/ColorMC.Launcher
 
-zip_name="colormc-a$version-macos-amd64.zip"
+zip_name="colormc-a$version-osx-x64.zip"
 
-cd ./src/build_out/osx64-dotnet
+cd ./src/build_out/osx-x64-dotnet
 zip -r $zip_name ./ColorMC.app
 mv $zip_name ../../../build_out/$zip_name
 
-echo "ColorMC macos-amd64 build done"
+echo "ColorMC osx-x64 build done"
