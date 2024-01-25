6de97da3a01d660fbc7a28976b5a1b2aa9325238
Color_yr <402067010@qq.com>
2024-01-25 16:03:26 +0800

    build test

--- build/build-windows.cmd
+++ build/build-windows.cmd
@@ -10,9 +10,9 @@ for /f %%i in ('type .\build\version') do (
 mkdir .\build_out
 
 call :build_win win-x64
-call :build_win win-arm64
-call :build_win_aot win-x64
-call :build_win_aot win-arm64
+@REM call :build_win win-arm64
+@REM call :build_win_aot win-x64
+@REM call :build_win_aot win-arm64
 goto :eof
 
 :build_win
@@ -25,15 +25,15 @@ mkdir .\src\build_out\%1-dotnet\colormc
 set "files=ColorMC.Gui.pdb ColorMC.Core.pdb Live2DCSharpSDK.App.pdb Live2DCSharpSDK.Framework.pdb ColorMC.Launcher.pdb ColorMC.Launcher.exe"
 
 for %%f in (%files%) do (
-    move .\src\build_out\%1-dotnet\%%f .\src\build_out\%1-dotnet\colormc\%%f
+    copy .\src\build_out\%1-dotnet\%%f .\src\build_out\%1-dotnet\colormc\%%f
 )
 
-set "zip_name=colormc-a%version%-%1.zip"
+@REM set "zip_name=colormc-a%version%-%1.zip"
 
-cd .\src\build_out\%1-dotnet\
-powershell Compress-Archive -Path .\colormc -DestinationPath ..\..\..\build_out\%zip_name% -Force
+@REM cd .\src\build_out\%1-dotnet\
+@REM powershell Compress-Archive -Path .\colormc -DestinationPath ..\..\..\build_out\%zip_name% -Force
 
-cd ..\..\..\
+@REM cd ..\..\..\
 
 echo ColorMC %1 build done
 goto :eof
@@ -48,15 +48,15 @@ mkdir .\src\build_out\%1-aot\colormc
 set "files=ColorMC.Launcher.exe av_libglesv2.dll libHarfBuzzSharp.dll libSkiaSharp.dll SDL2.dll"
 
 for %%f in (%files%) do (
-    move .\src\build_out\%1-aot\%%f .\src\build_out\%1-aot\colormc\%%f
+    copy .\src\build_out\%1-aot\%%f .\src\build_out\%1-aot\colormc\%%f
 )
 
-set "zip_name=colormc-a%version%-%1-aot.zip"
+@REM set "zip_name=colormc-a%version%-%1-aot.zip"
 
-cd .\src\build_out\%1-aot\
-powershell Compress-Archive -Path .\colormc -DestinationPath ..\..\..\build_out\%zip_name% -Force
+@REM cd .\src\build_out\%1-aot\
+@REM powershell Compress-Archive -Path .\colormc -DestinationPath ..\..\..\build_out\%zip_name% -Force
 
-cd ..\..\..\
+@REM cd ..\..\..\
 
 echo ColorMC %1-aot build done
 goto :eof
