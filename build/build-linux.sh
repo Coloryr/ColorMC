ea261cc64992f8811cd17a6ab86105cadb56c6e7
Color_yr <402067010@qq.com>
2024-01-25 12:33:37 +0800

    up

---
+++ build/build-linux.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+version=""
+
+for line in `cat ./build/version`
+do
+    version=$line
+done
+
+mkdir ./build_out
+
+build_linux() {
+
+    echo "build ColorMC-$1 version: $version"
+
+    dotnet publish ./src/ColorMC.Launcher -p:PublishProfile=$1
+
+    echo "ColorMC $1 build done"
+}
+
+build_linux_aot() {
+
+    echo "build ColorMC-$1-aot version: $version"
+
+    dotnet publish ./src/ColorMC.Launcher -p:PublishProfile=$1-aot
+
+    echo "ColorMC-$1-aot build done"
+}
+
+build_linux linux-x64
+build_linux linux-arm64
+# build_linux_aot linux-x64
+# build_linux_aot linux-arm64
