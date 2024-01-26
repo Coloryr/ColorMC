9da20a3a5aadd4b52859ba1b929eacbceafabba4
Color_yr <402067010@qq.com>
2024-01-26 08:29:33 +0800

    up

--- src/ColorMC.Setup.Wix/Program.cs
+++ src/ColorMC.Setup.Wix/Program.cs
@@ -48,7 +48,56 @@ namespace ColorMC.Setup.Wix
             project.ControlPanelInfo.Manufacturer = "Coloryr";
             project.ControlPanelInfo.InstallLocation = "[INSTALLDIR]";
 
-            //custom set of UI WPF dialogs
+            project.ManagedUI = new ManagedUI();
+
+            project.ManagedUI.InstallDialogs.Add<WelcomeDialog>()
+                                            .Add<ProgressDialog>()
+                                            .Add<ExitDialog>();
+
+            project.ManagedUI.ModifyDialogs.Add<MaintenanceTypeDialog>()
+                                           .Add<ProgressDialog>()
+                                           .Add<ExitDialog>();
+            project.OutFileName = "ColorMC";
+            project.BuildMsi();
+
+            project = new ManagedProject("ColorMC",
+                        new Dir(@"%ProgramFiles%\ColorMC",
+                            new File(@"..\build_out\win-x64-aot\ColorMC.Launcher.exe", 
+                                new FileShortcut("ColorMC") { WorkingDirectory = "[INSTALLDIR]" }),
+                            new File(@"..\build_out\win-x64-aot\av_libglesv2.dll"),
+                            new File(@"..\build_out\win-x64-aot\libHarfBuzzSharp.dll"),
+                            new File(@"..\build_out\win-x64-aot\libSkiaSharp.dll"),
+                            new File(@"..\build_out\win-x64-aot\SDL2.dll"),
+                            new ExeFileShortcut("ColorMC Setting", "[System64Folder]msiexec.exe", "/i [ProductCode]")),
+                        new Dir(@"%ProgramMenu%\ColorMC",
+                            new ExeFileShortcut("ColorMC Setting", "[System64Folder]msiexec.exe", "/i [ProductCode]"),
+                            new ExeFileShortcut("ColorMC", "[INSTALLDIR]ColorMC.Launcher.exe", arguments: "")),
+                        new Dir(@"%Desktop%",
+                            new ExeFileShortcut("ColorMC", "[INSTALLDIR]ColorMC.Launcher.exe", arguments: "")
+                            {
+                                Condition = new Condition("INSTALLDESKTOPSHORTCUT=\"yes\"") //property based condition
+                            }),
+                        //setting property to be used in install condition
+                        new Property("INSTALLDESKTOPSHORTCUT", "yes"),
+                        new Property("REMOVE_ALL_FILE", "false"))
+            {
+                GUID = new Guid("BA2749D2-BBA4-4ACE-8E06-C4E100343C1A"),
+                Platform = Platform.x64,
+                BannerImage = "game.png",
+                BackgroundImage = "game.png",
+                Version = new Version(1, 24),
+                Description = "A Minecraft Launcher"
+            };
+
+            project.Scope = InstallScope.perMachine;
+
+            project.ControlPanelInfo.Comments = "ColorMC";
+            project.ControlPanelInfo.HelpLink = "https://github.com/Coloryr/ColorMC";
+            project.ControlPanelInfo.UrlInfoAbout = "https://github.com/Coloryr/ColorMC";
+            project.ControlPanelInfo.ProductIcon = "icon.ico";
+            project.ControlPanelInfo.Manufacturer = "Coloryr";
+            project.ControlPanelInfo.InstallLocation = "[INSTALLDIR]";
+
             project.ManagedUI = new ManagedUI();
 
             project.ManagedUI.InstallDialogs.Add<WelcomeDialog>()
@@ -59,9 +108,7 @@ namespace ColorMC.Setup.Wix
                                            .Add<ProgressDialog>()
                                            .Add<ExitDialog>();
 
-            //project.SourceBaseDir = "<input dir path>";
-            //project.OutDir = "<output dir path>";
-
+            project.OutFileName = "ColorMC-aot";
             project.BuildMsi();
         }
     }
