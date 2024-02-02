8021325b2390ed1ebe205d7fd38cb036c93ebd74
Color_yr <402067010@qq.com>
2024-02-02 10:04:16 +0800

    new!

--- src/ColorMC.Gui/UI/Flyouts/FlyoutsControl.axaml.cs
+++ src/ColorMC.Gui/UI/Flyouts/FlyoutsControl.axaml.cs
@@ -1,53 +1,30 @@
 using Avalonia.Controls;
-using Avalonia.Controls.Primitives;
 using System;
+using System.Collections.Generic;
 
 namespace ColorMC.Gui.UI.Flyouts;
 
-public partial class FlyoutsControl : UserControl
+public class FlyoutsControl
 {
-    public class SelfFlyout : PopupFlyoutBase
-    {
-        private readonly FlyoutsControl _con;
-        public SelfFlyout(FlyoutsControl con)
-        {
-            _con = con;
-        }
-        protected override Control CreatePresenter()
-        {
-            return _con;
-        }
-    }
-
     public FlyoutsControl((string, bool, Action)[] list, Control con)
     {
-        InitializeComponent();
-
-        var flyout = new SelfFlyout(this);
+        var flyout = new MenuFlyout();
+        var list1 = new List<MenuItem>();
         foreach (var item in list)
         {
-            var button = new ListBoxItem()
+            var button = new MenuItem()
             {
-                Content = item.Item1,
-                IsEnabled = item.Item2
+                Header = item.Item1,
+                IsEnabled = item.Item2,
             };
             button.PointerPressed += (a, b) =>
             {
                 flyout.Hide();
                 item.Item3.Invoke();
             };
-            StackPanel1.Children.Add(button);
+            list1.Add(button);
         }
-        flyout.ShowAt(con!, true);
-    }
-
-    public FlyoutsControl()
-    {
-        InitializeComponent();
-        var button = new ListBoxItem()
-        {
-            Content = "Test"
-        };
-        StackPanel1.Children.Add(button);
+        flyout.ItemsSource = list1;
+        flyout.ShowAt(con, true);
     }
 }
