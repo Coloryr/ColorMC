f0b04fa704519bd3db0865f460a49f1e6345eaed
Color_yr <26276037+Coloryr@users.noreply.github.com>
2024-02-27 21:14:24 +0800

    up 升级版本号至A24.1

--- src/ColorMC.Launcher/Program.cs
+++ src/ColorMC.Launcher/Program.cs
@@ -42,7 +42,7 @@ public static class Program
     /// <summary>
     /// 加载路径
     /// </summary>
-    public const string TopVersion = "A24";
+    public const string TopVersion = "A24.1";
 
     public static readonly string[] BaseSha1 =
     [
