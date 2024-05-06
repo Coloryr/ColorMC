676b11977e5fd1776c9364f44a52829bef2c96ab
Coloryr <402067010@qq.com>
2024-05-06 13:10:27 +0800

    up

--- src/ColorMC.Core/CoreMain.cs
+++ src/ColorMC.Core/CoreMain.cs
@@ -17,7 +17,7 @@ namespace ColorMC.Core;
 public static class ColorMCCore
 {
     public const string TopVersion = "A25";
-    public const string DateVersion = "20240425";
+    public const string DateVersion = "20240506";
 
     public const string Version = $"{TopVersion}.{DateVersion}";
 
