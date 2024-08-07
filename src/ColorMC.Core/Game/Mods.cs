2d20e97a8e7d2abbc7e3088fd0d0bcd282afee8d
Coloryr <402067010@qq.com>
2024-08-07 15:27:57 +0800

    up 修改模组解析

--- src/ColorMC.Core/Game/Mods.cs
+++ src/ColorMC.Core/Game/Mods.cs
@@ -6,6 +6,7 @@ using ColorMC.Core.Objs;
 using ColorMC.Core.Objs.Minecraft;
 using ColorMC.Core.Utils;
 using ICSharpCode.SharpZipLib.Zip;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Tomlyn;
 using Tomlyn.Model;
@@ -36,11 +37,11 @@ public static class Mods
         var files = info.GetFiles();
 
         //多线程同时检查
-        //await Parallel.ForEachAsync(files, new ParallelOptions()
-        //{
-        //    MaxDegreeOfParallelism = 1
-        //}, async (item, cancel) =>
-        await Parallel.ForEachAsync(files, async (item, cancel) =>
+        await Parallel.ForEachAsync(files, new ParallelOptions()
+        {
+            MaxDegreeOfParallelism = 1
+        }, async (item, cancel) =>
+        //await Parallel.ForEachAsync(files, async (item, cancel) =>
         {
             if (item.Extension is not (".zip" or ".jar" or ".disable" or ".disabled"))
             {
@@ -294,16 +295,18 @@ public static class Mods
         {
             if (item.Key == key)
             {
-                return item.Value!.Root;
+                return obj;
             }
 
-            if (item.Value is JObject obj1)
+            if (item.Value is JObject obj1
+                && FindKey(obj1, key) is { } obj2)
             {
-                return FindKey(obj1, key);
+                return obj2;
             }
-            else if (item.Value is JArray arry)
+            else if (item.Value is JArray arry
+                && FindKey(arry, key) is { } obj3)
             {
-                return FindKey(arry, key);
+                return obj3;
             }
         }
 
@@ -316,7 +319,7 @@ public static class Mods
     /// <param name="obj"></param>
     /// <param name="key">键名</param>
     /// <returns>obj</returns>
-    private static JContainer? FindKey(this JArray obj, string key)
+    private static JToken? FindKey(this JArray obj, string key)
     {
         foreach (var item in obj)
         {
@@ -325,12 +328,33 @@ public static class Mods
                 var data = FindKey(obj1, key);
                 if (data != null)
                 {
-                    return data.Parent;
+                    return data;
                 }
             }
         }
 
         return null;
+    }
+
+    private static async Task JarInJar(ModObj obj, ZipFile zFile)
+    {
+        obj.InJar ??= [];
+        foreach (ZipEntry item3 in zFile)
+        {
+            if (item3.Name.EndsWith(".jar") && item3.Name.StartsWith("META-INF/jarjar/"))
+            {
+                using var filestream = zFile.GetInputStream(item3);
+                using var stream2 = new MemoryStream();
+                await filestream.CopyToAsync(stream2);
+                stream2.Seek(0, SeekOrigin.Begin);
+                using var zFile1 = new ZipFile(stream2);
+                var inmod = await ReadModAsync(zFile1);
+                if (inmod != null)
+                {
+                    obj.InJar.Add(inmod);
+                }
+            }
+        }
     }
 
     /// <summary>
@@ -348,7 +372,7 @@ public static class Mods
             using var stream2 = new MemoryStream();
             await stream1.CopyToAsync(stream2);
             var data = Encoding.UTF8.GetString(stream2.ToArray());
-            var obj1 = JToken.Parse(data);
+            var obj1 = Parse(data);
             JObject obj3;
             if (obj1 is JArray array)
             {
@@ -367,38 +391,6 @@ public static class Mods
                 obj4.Loader = Loaders.Forge;
                 return obj4;
             }
-        }
-
-        //forge coremod
-        item1 = zFile.GetEntry("META-INF/fml_cache_annotation.json");
-        if (item1 != null)
-        {
-            using var stream1 = zFile.GetInputStream(item1);
-            using var stream2 = new MemoryStream();
-            await stream1.CopyToAsync(stream2);
-            var data = Encoding.UTF8.GetString(stream2.ToArray());
-            var obj1 = JObject.Parse(data);
-            var obj2 = FindKey(obj1, "acceptedMinecraftVersions");
-            if (obj2 == null)
-            {
-                return null;
-            }
-
-            var obj3 = new ModObj
-            {
-                V2 = true,
-                Loader = Loaders.Forge,
-                modid = obj2["modId"]?["value"]?.ToString() ?? "",
-                name = obj2["names"]?["value"]?.ToString() ?? "",
-                version = obj2["version"]?["value"]?.ToString(),
-                requiredMods = [obj2["dependencies"]?["value"]?.ToString() ?? ""],
-                CoreMod = true
-            };
-
-            obj3.modid ??= "";
-            obj3.name ??= obj3.modid;
-
-            return obj3;
         }
 
         //forge 1.13及以上
@@ -485,23 +477,7 @@ public static class Mods
                 }
             }
 
-            obj3.InJar = [];
-            foreach (ZipEntry item3 in zFile)
-            {
-                if (item3.Name.EndsWith(".jar") && item3.Name.StartsWith("META-INF/jarjar/"))
-                {
-                    using var filestream = zFile.GetInputStream(item3);
-                    using var stream2 = new MemoryStream();
-                    await filestream.CopyToAsync(stream2);
-                    stream2.Seek(0, SeekOrigin.Begin);
-                    using var zFile1 = new ZipFile(stream2);
-                    var inmod = await ReadModAsync(zFile1);
-                    if (inmod != null)
-                    {
-                        obj3.InJar.Add(inmod);
-                    }
-                }
-            }
+            await JarInJar(obj3, zFile);
 
             return obj3;
         }
@@ -541,25 +517,7 @@ public static class Mods
                 }
             }
 
-            //JarInJar
-            obj3.InJar = [];
-
-            foreach (ZipEntry item2 in zFile)
-            {
-                if (item2.Name.EndsWith(".jar") && item2.Name.StartsWith("META-INF/jars/"))
-                {
-                    using var filestream = zFile.GetInputStream(item2);
-                    using var stream2 = new MemoryStream();
-                    await filestream.CopyToAsync(stream2);
-                    stream2.Seek(0, SeekOrigin.Begin);
-                    using var zFile1 = new ZipFile(stream2);
-                    var inmod = await ReadModAsync(zFile1);
-                    if (inmod != null)
-                    {
-                        obj3.InJar.Add(inmod);
-                    }
-                }
-            }
+            await JarInJar(obj3, zFile);
 
             return obj3;
         }
@@ -603,27 +561,175 @@ public static class Mods
                 }
             }
 
-            //JarInJar
-            obj3.InJar = [];
-
-            foreach (ZipEntry item2 in zFile)
-            {
-                if (item2.Name.EndsWith(".jar") && item2.Name.StartsWith("META-INF/jars/"))
-                {
-                    using var filestream = zFile.GetInputStream(item2);
-                    using var zFile1 = new ZipFile(filestream);
-                    var inmod = await ReadModAsync(zFile1);
-                    if (inmod != null)
-                    {
-                        obj3.InJar.Add(inmod);
-                    }
-                }
-            }
+            await JarInJar(obj3, zFile);
 
             return obj3;
+        }
+
+        //core mod
+        item1 = zFile.GetEntry("META-INF/services/cpw.mods.modlauncher.api.ITransformationService");
+        var item5 = zFile.GetEntry("META-INF/services/net.minecraftforge.forgespi.language.IModLanguageProvider");
+        var item6 = zFile.GetEntry("META-INF/MANIFEST.MF");
+        if (item6 != null)
+        {
+            using var stream12 = zFile.GetInputStream(item6);
+            using var reader = new StreamReader(stream12);
+            var con = Options.ReadOptions(reader.ReadToEnd());
+            if (item1 != null || item5 != null)
+            {
+                if (!con.TryGetValue("Specification-Title", out string? name)
+                    && !con.TryGetValue("Implementation-Title", out name)
+                    && !con.TryGetValue("Automatic-Module-Name", out name))
+                {
+                    name = "";
+                }
+                name = name.Trim();
+                var obj3 = new ModObj
+                {
+                    V2 = true,
+                    Loader = Loaders.Forge,
+                    name = name,
+                    modid = name.ToLower(),
+                    CoreMod = true
+                };
+
+                await JarInJar(obj3, zFile);
+
+                return obj3;
+            }
+            else if (con.TryGetValue("FMLCorePlugin", out string? fml))
+            {
+                fml = fml.Trim();
+                return new ModObj
+                {
+                    V2 = true,
+                    Loader = Loaders.Forge,
+                    name = fml,
+                    modid = fml.ToLower(),
+                    CoreMod = true
+                };
+            }
+        }
+
+        item1 = zFile.GetEntry("META-INF/fml_cache_annotation.json");
+        //forge coremod
+        if (item1 != null)
+        {
+            using var stream1 = zFile.GetInputStream(item1);
+            using var stream2 = new MemoryStream();
+            await stream1.CopyToAsync(stream2);
+            var data = Encoding.UTF8.GetString(stream2.ToArray());
+            var obj1 = JObject.Parse(data);
+            var obj2 = FindKey(obj1, "acceptedMinecraftVersions");
+            if (obj2 == null)
+            {
+                return null;
+            }
+
+            var obj3 = new ModObj
+            {
+                V2 = true,
+                Loader = Loaders.Forge,
+                CoreMod = true
+            };
+
+            if (obj2["modId"]?["value"]?.ToString() is { } str)
+            {
+                obj3.modid = str;
+            }
+            else if (obj2["modid"]?["value"]?.ToString() is { } str1)
+            {
+                obj3.modid = str1;
+            }
+            else
+            {
+                obj3.modid = "";
+            }
+
+            if (obj2["names"]?["value"]?.ToString() is { } str2)
+            {
+                obj3.name = str2;
+            }
+            else if (obj2["name"]?["value"]?.ToString() is { } str3)
+            {
+                obj3.name = str3;
+            }
+            else
+            {
+                obj3.name = "";
+            }
+
+            if (obj2["version"]?["value"]?.ToString() is { } str4)
+            {
+                obj3.version = str4;
+            }
+            else
+            {
+                obj3.version = "";
+            }
+
+            if (obj2["dependencies"]?["value"]?.ToString() is { } str5)
+            {
+                obj3.requiredMods = [str5];
+            }
+            else
+            {
+                obj3.requiredMods = [];
+            }
+
+            obj3.name ??= obj3.modid;
+
+            return obj3;
+        }
+
+        //使用jarjar的内容
+        foreach (ZipEntry item3 in zFile)
+        {
+            if (item3.Name.EndsWith(".jar") && item3.Name.StartsWith("META-INF/jarjar/"))
+            {
+                using var filestream = zFile.GetInputStream(item3);
+                using var stream2 = new MemoryStream();
+                await filestream.CopyToAsync(stream2);
+                stream2.Seek(0, SeekOrigin.Begin);
+                using var zFile1 = new ZipFile(stream2);
+                var inmod = await ReadModAsync(zFile1);
+                if (inmod != null
+                    && !string.IsNullOrWhiteSpace(inmod.name)
+                     && !string.IsNullOrWhiteSpace(inmod.modid)
+                     && !inmod.CoreMod)
+                {
+                    return inmod;
+                }
+            }
         }
 
         return null;
     }
 
+    private static JToken Parse(string data)
+    {
+        try
+        {
+            return JToken.Parse(data);
+        }
+        catch (Exception)
+        {
+            var lines = data.Split(new[] { '\n' }, StringSplitOptions.None);
+            var token = new JObject();
+            foreach (var line in lines)
+            {
+                try
+                {
+                    JToken parsedLine = JToken.Parse("{" + line + "}");
+                    token.Add(parsedLine.First);
+                }
+                catch (Exception)
+                {
+                    
+                }
+            }
+
+            return new JArray() { token };
+        }
+    }
 }
