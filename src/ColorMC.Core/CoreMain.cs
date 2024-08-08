aef56026603be9fb7795caa08a6f980149f202df
Coloryr <402067010@qq.com>
2024-08-08 11:40:39 +0800

    up

--- src/ColorMC.Core/CoreMain.cs
+++ src/ColorMC.Core/CoreMain.cs
@@ -15,7 +15,7 @@ namespace ColorMC.Core;
 public static class ColorMCCore
 {
     public const string TopVersion = "A28";
-    public const string DateVersion = "20240805";
+    public const string DateVersion = "20240808";
 
     /// <summary>
     /// 版本号
