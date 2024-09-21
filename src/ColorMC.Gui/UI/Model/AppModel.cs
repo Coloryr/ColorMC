007667a73ca53de0f49a789cfeabcb3a7c68277a
Color_yr <402067010@qq.com>
2024-09-22 00:19:30 +0800

    up  调整macos兼容

---
+++ src/ColorMC.Gui/UI/Model/AppModel.cs
@@ -0,0 +1,39 @@
+using ColorMC.Gui.Manager;
+using ColorMC.Gui.Objs;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace ColorMC.Gui.UI.Model;
+
+public partial class AppModel : ObservableObject
+{
+    [RelayCommand]
+    public void ShowMain()
+    {
+        WindowManager.ShowMain();
+    }
+
+    [RelayCommand]
+    public void ShowSetting()
+    {
+        WindowManager.ShowSetting(SettingType.Normal);
+    }
+
+    [RelayCommand]
+    public void AddUser()
+    {
+        WindowManager.ShowUser(true);
+    }
+
+    [RelayCommand]
+    public void AddGame()
+    {
+        WindowManager.ShowAddGame(null);
+    }
+
+    [RelayCommand]
+    public void Exit()
+    {
+        App.Close();
+    }
+}
