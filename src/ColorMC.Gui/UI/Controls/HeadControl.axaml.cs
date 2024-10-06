538b2b83dbf4dbd030b29dd794397c6c5645cdff
Coloryr <402067010@qq.com>
2024-10-06 10:09:39 +0800

    up 恢复macos样式

--- src/ColorMC.Gui/UI/Controls/HeadControl.axaml.cs
+++ src/ColorMC.Gui/UI/Controls/HeadControl.axaml.cs
@@ -4,8 +4,10 @@ using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
 using Avalonia.Media;
+using ColorMC.Core.Objs;
+using ColorMC.Core.Utils;
 using ColorMC.Gui.UI.Model;
-
+using Avalonia.Layout;
 
 namespace ColorMC.Gui.UI.Controls;
 
@@ -37,43 +39,115 @@ public partial class HeadControl : UserControl
 
         DataContextChanged += HeadControl_DataContextChanged;
 
-        _buttonMin = new Button()
+        if (SystemInfo.Os != OsType.MacOS)
         {
-            Width = 40,
-            Height = 35,
-            Content = new HeadImg() { Path = "/Resource/Icon/Head/min.svg" },
-            BorderThickness = new Thickness(0),
-            BorderBrush = Brushes.Transparent,
-            CornerRadius = new CornerRadius(0),
-            Background = Brushes.Transparent
-        };
-        _buttonMax = new Button()
-        {
-            Width = 40,
-            Height = 35,
-            BorderThickness = new Thickness(0),
-            BorderBrush = Brushes.Transparent,
-            CornerRadius = new CornerRadius(0),
-            Background = Brushes.Transparent
-        };
-        var max = new HeadImg();
-        max.Bind(HeadImg.PathProperty, MaxObservale.ToBinding());
-        MaxObservale.Notify(MaxIcon[0]);
-        _buttonMax.Content = max;
-        _buttonClose = new Button()
-        {
-            Width = 40,
-            Height = 35,
-            Content = new HeadImg() { Path = "/Resource/Icon/Head/close.svg" },
-            BorderThickness = new Thickness(0),
-            BorderBrush = Brushes.Transparent,
-            CornerRadius = new CornerRadius(0),
-            Background = Brushes.Transparent
-        };
+            StackPanel2.HorizontalAlignment = HorizontalAlignment.Center;
+            Icons.IsVisible = false;
+            var img1 = new HeadImg() { IsVisible = false, Path = "/Resource/Icon/Head/min.svg" };
+            _buttonMin = new Button()
+            {
+                Width = 15,
+                Height = 15,
+                Content = img1,
+                Margin = new Thickness(25,0,0,0),
+                VerticalAlignment = VerticalAlignment.Center,
+                BorderThickness = new Thickness(0),
+                BorderBrush = Brushes.Transparent,
+                CornerRadius = new CornerRadius(20),
+                Background = Brush.Parse("#febb2c")
+            };
+            _buttonMin.PointerEntered += (a, b) => { img1.IsVisible = true; };
+            _buttonMin.PointerExited += (a, b) => { img1.IsVisible = false; };
 
-        StackPanel1.Children.Add(_buttonMin);
-        StackPanel1.Children.Add(_buttonMax);
-        StackPanel1.Children.Add(_buttonClose);
+            var max = new HeadImg()
+            {
+                IsVisible = false
+            };
+            max.Bind(HeadImg.PathProperty, MaxObservale.ToBinding());
+            MaxObservale.Notify(MaxIcon[0]);
+            _buttonMax = new Button()
+            {
+                Width = 15,
+                Height = 15,
+                Content = max,
+                Margin = new Thickness(50, 0, 0, 0),
+                VerticalAlignment = VerticalAlignment.Center,
+                BorderThickness = new Thickness(0),
+                BorderBrush = Brushes.Transparent,
+                CornerRadius = new CornerRadius(20),
+                Background = Brush.Parse("#29c73f")
+            };
+            _buttonMax.PointerEntered += (a, b) => { max.IsVisible = true; };
+            _buttonMax.PointerExited += (a, b) => { max.IsVisible = false; };
+
+            var img2 = new HeadImg() { IsVisible = false, Path = "/Resource/Icon/Head/close.svg" };
+            _buttonClose = new Button()
+            {
+                Width = 15,
+                Height = 15,
+                Content = img2,
+                Margin = new Thickness(5, 0, 0, 0),
+                VerticalAlignment = VerticalAlignment.Center,
+                BorderThickness = new Thickness(0),
+                BorderBrush = Brushes.Transparent,
+                CornerRadius = new CornerRadius(20),
+                Background = Brush.Parse("#fe5f59")
+            };
+            _buttonClose.PointerEntered += (a, b) => { img2.IsVisible = true; };
+            _buttonClose.PointerExited += (a, b) => { img2.IsVisible = false; };
+
+            Panel1.Children.Add(_buttonClose);
+            Panel1.Children.Add(_buttonMin);
+            Panel1.Children.Add(_buttonMax);
+
+            Pandl2.Children.Remove(StackPanel2);
+            Panel1.Children.Add(StackPanel2);
+
+            StackPanel1.HorizontalAlignment = HorizontalAlignment.Right;
+            StackPanel2.MaxWidth = 500;
+            StackPanel2.HorizontalAlignment = HorizontalAlignment.Center;
+        }
+        else
+        {
+            _buttonMin = new Button()
+            {
+                Width = 40,
+                Height = 35,
+                Content = new HeadImg() { Path = "/Resource/Icon/Head/min.svg" },
+                BorderThickness = new Thickness(0),
+                BorderBrush = Brushes.Transparent,
+                CornerRadius = new CornerRadius(0),
+                Background = Brushes.Transparent
+            };
+            _buttonMax = new Button()
+            {
+                Width = 40,
+                Height = 35,
+                BorderThickness = new Thickness(0),
+                BorderBrush = Brushes.Transparent,
+                CornerRadius = new CornerRadius(0),
+                Background = Brushes.Transparent
+            };
+            var max = new HeadImg();
+            max.Bind(HeadImg.PathProperty, MaxObservale.ToBinding());
+            MaxObservale.Notify(MaxIcon[0]);
+            _buttonMax.Content = max;
+            _buttonClose = new Button()
+            {
+                Width = 40,
+                Height = 35,
+                Content = new HeadImg() { Path = "/Resource/Icon/Head/close.svg" },
+                BorderThickness = new Thickness(0),
+                BorderBrush = Brushes.Transparent,
+                CornerRadius = new CornerRadius(0),
+                Background = Brushes.Transparent
+            };
+
+            StackPanel1.Children.Add(_buttonMin);
+            StackPanel1.Children.Add(_buttonMax);
+            StackPanel1.Children.Add(_buttonClose);
+        }
+
 
         _buttonMin.Click += ButtonMin_Click;
         _buttonMax.Click += ButtonMax_Click;
