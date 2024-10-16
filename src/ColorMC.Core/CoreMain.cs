4e8c2a0184eed92dc03ace55049fbbc6ea738420
Coloryr <402067010@qq.com>
2024-10-16 16:32:48 +0800

    up 修复启动崩溃问题

--- src/ColorMC.Core/CoreMain.cs
+++ src/ColorMC.Core/CoreMain.cs
@@ -15,7 +15,7 @@ namespace ColorMC.Core;
 public static class ColorMCCore
 {
     public const string TopVersion = "A31";
-    public const string DateVersion = "20241012";
+    public const string DateVersion = "20241016";
 
     /// <summary>
     /// 版本号
