546eb15dce268a87f3eab4058f03b25788c148ef
coloryr <402067010@qq.com>
2024-11-04 15:57:44 +0800

    up

--- build/aur/makeinfo.sh
+++ build/aur/makeinfo.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env bash
 
-VERSION=31
-URL='https://github.com/Coloryr/ColorMC/releases/download/a31.2024.10.10/colormc-linux-a31-1-x86_64.pkg.tar.zst'
+VER=31
+REL=3
+URL='https://github.com/Coloryr/ColorMC/releases/download/a31.2024.10.16/colormc-linux-a31-1-x86_64.pkg.tar.zst'
 TARGET='colormc-linux-a31-1-x86_64.pkg.tar.zst'
 POSTURL=$(echo $URL | sed 's/\//\\\//g')
 wget $URL -O $TARGET
 SUM=$(sha256sum $TARGET | cut -f1 -d' ')
 
-sed -i 's/^source=(.*)/source=('"'"$POSTURL"'"')/' PKGBUILD
-sed -i 's/^sha256sums=(.*)/sha256sums=('"'"$SUM"'"')/' PKGBUILD
-sed -i 's/^pkgver=.*/pkgver='$VERSION'/' PKGBUILD
+rm -rf out
+mkdir out
+cp PKGBUILD_Pre out/PKGBUILD
+cp colormc.install out/colormc.install
+
+cd out
+
+sed -i "s/%ver%/$VER/g" PKGBUILD
+sed -i "s/%rel%/$REL/g" PKGBUILD
+sed -i "s/%source%/$POSTURL/g" PKGBUILD
+sed -i "s/%sha%/$SUM/g" PKGBUILD
+sed -i "s/%file%/$TARGET/g" PKGBUILD
 
 makepkg --printsrcinfo > .SRCINFO
+cd ../
