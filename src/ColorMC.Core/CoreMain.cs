abb5b5d542f8009732e86a61ba894ffd84d7dc89
Color_yr <402067010@qq.com>
2024-11-14 16:11:35 +0800

    up 更新版本号

--- src/ColorMC.Core/CoreMain.cs
+++ src/ColorMC.Core/CoreMain.cs
@@ -15,7 +15,7 @@ namespace ColorMC.Core;
 public static class ColorMCCore
 {
     public const string TopVersion = "A33";
-    public const string DateVersion = "20241113";
+    public const string DateVersion = "20241114";
 
     /// <summary>
     /// 版本号
