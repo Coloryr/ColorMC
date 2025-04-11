acb14a72a3e9944ece3445038afa8260988acde0
Color_yr <402067010@qq.com>
2025-04-11 08:50:15 +0800

    Update CoreMain.cs

--- src/ColorMC.Core/CoreMain.cs
+++ src/ColorMC.Core/CoreMain.cs
@@ -15,7 +15,7 @@ public static class ColorMCCore
 {
     public const int VersionNum = 36;
     public const string TopVersion = "A36";
-    public const string DateVersion = "20250410";
+    public const string DateVersion = "20250411";
 
     /// <summary>
     /// 版本号
