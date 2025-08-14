1315c306fd3071889bb3fc6abb5095ffc3c7d045
Color_yr <402067010@qq.com>
2025-08-14 10:31:14 +0800

    up

--- src/ColorMC.Launcher/Program.cs
+++ src/ColorMC.Launcher/Program.cs
@@ -50,7 +50,7 @@ public static class Program
     /// <summary>
     /// 加载路径
     /// </summary>
-    public const string TopVersion = "A38";
+    public const string TopVersion = "A39";
 
     public static readonly string[] BaseSha1 =
     [
