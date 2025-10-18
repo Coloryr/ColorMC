52077fbe6763498f98ceadcde65295f56b744723
Color_yr <402067010@qq.com>
2025-10-18 13:14:14 +0800

    up

---
+++ src/ColorMC.Gui/UI/Windows/TestWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ColorMC.Gui;
+
+public partial class TestWindow : Window
+{
+    public TestWindow()
+    {
+        InitializeComponent();
+    }
+}
