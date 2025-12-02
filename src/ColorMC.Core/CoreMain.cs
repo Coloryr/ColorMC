49744781866e15ca33dfbbe654bc3dcf51e37db3
Color_yr <402067010@qq.com>
2025-12-02 10:06:41 +0800

    up log
    更新日志

--- src/ColorMC.Core/CoreMain.cs
+++ src/ColorMC.Core/CoreMain.cs
@@ -15,7 +15,7 @@ public static class ColorMCCore
 {
     public const int VersionNum = 39;
     public const string TopVersion = "39";
-    public const string DateVersion = "20251022";
+    public const string DateVersion = "20251202";
 
     /// <summary>
     /// 版本号
